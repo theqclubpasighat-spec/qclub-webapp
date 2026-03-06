98df8f1dbde1556071ba5ca225bfa0c7f8463415
theqclubpasighat-spec <the.q.club.pasighat@gmail.com>
2026-03-06 09:20:25 +0530

    Add files via upload
    enable PWA

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { registerSW } from 'virtual:pwa-register'
+//import { registerSW } from 'virtual:pwa-register'
 import App from "./App.jsx";
 import "./styles.css";
 
