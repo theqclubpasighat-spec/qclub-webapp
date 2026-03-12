0dddb4a3481ffffa831d891a50ff13beb0e97a12
theqclubpasighat-spec <the.q.club.pasighat@gmail.com>
2026-03-12 23:32:29 +0530

    Add files via upload
    Update Cashfree sandbox API

--- api/create-order.js
+++ api/create-order.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
 
   const order_id = "order_" + Date.now();
 
-  const response = await fetch("https://api.cashfree.com/pg/orders", {
+  const response = await fetch("https://sandbox.cashfree.com/pg/orders", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
