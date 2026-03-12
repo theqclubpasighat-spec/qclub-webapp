2e08f652d2155671b775631a3d53eeadff15a48b
theqclubpasighat-spec <the.q.club.pasighat@gmail.com>
2026-03-12 23:14:15 +0530

    Add files via upload
    enable PWA

--- src/App.jsx
+++ src/App.jsx
@@ -633,7 +633,7 @@ export default function App() {
 
       if (data?.payment_session_id) {
         const cashfree = window.Cashfree({
-          mode: "production",
+          mode: "sandbox",
         });
 
         cashfree.checkout({
